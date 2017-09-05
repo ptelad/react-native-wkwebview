9a81ecc827ad6878d91ac020c85af83ddae463ae
Elad Gil <elad@helleko.com>
2017-09-05 14:40:34 +0300

    disabled zoom function

--- ios/RCTWKWebView/RCTWKWebView.m
+++ ios/RCTWKWebView/RCTWKWebView.m
@@ -369,6 +369,10 @@ RCT_NOT_IMPLEMENTED(- (instancetype)initWithCoder:(NSCoder *)aDecoder)
   else if (_onLoadingFinish && !webView.loading && ![webView.URL.absoluteString isEqualToString:@"about:blank"]) {
     _onLoadingFinish([self baseEvent]);
   }
+
+  NSString *javascript = @"var meta = document.createElement('meta');meta.setAttribute('name', 'viewport');meta.setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no');document.getElementsByTagName('head')[0].appendChild(meta);";
+  
+  [webView evaluateJavaScript:javascript completionHandler:nil];
 }
 
 #pragma mark - WKUIDelegate
